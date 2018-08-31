ad677f965a96191c6f39ec543324d0abc551925d
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-08-31 08:11:43 -0500

    Add mail functionality to send form page

--- app.js
+++ app.js
@@ -35,6 +35,38 @@ app.get("/contact", function (request, response) {
   response.render('contact');
 });
 
+app.get("/sendform", function (request, response) {
+   
+  response.sendfile(path.join(__dirname +"/views/sendform.html"));
+});
+
+
+app.post("/sendform", function(request,response){
+  console.log("checking mail")
+  var api_key = 'key-770c3cc90056cea80af1cafa3f4079cb';
+  var domain = 'sandbox520a2a03c8ae42dcb83afd7b3e7ffdad.mailgun.org';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+   
+  var data = {
+    from: 'Mail Gun <postmaster@sandbox520a2a03c8ae42dcb83afd7b3e7ffdad.mailgun.org>',
+    to: 'S530489@nwmissouri.edu',
+    subject: "Mail from Stephie Costume Desinger",
+
+    text : "Hi " +request.body.firstname+" "+request.body.lastname+"\n"
+    +"please click the below link and fill the measurement form"+"\n\n"+
+
+    "http://firebase/fillmeasurementform.html"
+  };
+   
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+    if(!error)
+      response.send("mail sent sucessfully");
+    else
+      response.send("mail not sent");
+
+  });
+});
 
 
   http.listen(8081, function () {
