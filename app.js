66384c6fe647d7c3a819316795aa26a2df017ce8
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-04 22:15:33 -0500

    fix the error "Cannot read property  undefined" in the send form page

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var path = require("path");
+
 var express = require("express");
 var logger = require("morgan");
 var bodyParser = require("body-parser"); // simplifies access to request body
 var app = express();  // make express app
 var http = require('http').Server(app);  // inject app into the server
 
-const routes = require('./routes/index.js');
-app.use('/', routes);
+
 
 // set up the view engine
 app.set("views", path.resolve(__dirname, "views")); // path to views
@@ -18,9 +18,14 @@ app.locals.entries = entries;
 
 // set up an http request logger to log every request automagically
 app.use(logger("dev"));     // app.use() establishes middleware functions
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const routes = require('./routes/index.js');
+app.use('/', routes);
+
 
 // handle http GET requests (default)
 
