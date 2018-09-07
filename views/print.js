a63841e58d4d509b9e238e411cf70e9c5fd36897
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-07 12:48:51 -0500

    Add text and styling to shop pull list pdf

--- views/print.js
+++ views/print.js
@@ -1,11 +1,18 @@
 
+// link for jspdf documentation https://mrrio.github.io/
+
 function genPDF(){
     html2canvas(document.getElementById("dplist"),{
         onrendered: function (canvas){
-            var img = canvas.toDataURL("image/png",1.0);
+            var img = canvas.toDataURL("image/png");
             var doc = new jsPDF("1","mm","a3");
-            
-            doc.addImage(img, 'JPEG', 20, 20);
+            doc.setFont("courier");
+            doc.setFontType("bolditalic");
+            doc.setFontSize(40);
+            doc.setTextColor(26, 140, 255);
+            doc.text( 'SHOP PULL LIST',90,28,null,null);
+    
+            doc.addImage(img, 'JPEG', 20,38,);
             doc.save("ShopPullList.pdf");
         }
     });
