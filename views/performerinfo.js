65d23db656b787389bd0c8088a924c8ed692957e
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-10 09:45:55 -0500

    Fix the path for images in the public folder

--- views/performerinfo.js
+++ views/performerinfo.js
@@ -2,7 +2,7 @@ window.alert("test")
 
 console.log('test')
 
-var data = JSON.parse(performerInfo.JSON);
+//var data = JSON.parse(performerInfo.JSON);
 console.log("test")
 window.alert("test")
 console.log(data);
