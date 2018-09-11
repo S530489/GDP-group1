4f81479215df56eec2f69fd1d562bd6544425df8
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-11 11:07:58 -0500

    Fix the function calling error in performer info page

--- views/performerinfo.js
+++ views/performerinfo.js
@@ -1,6 +1,20 @@
 function getInfo(key, performer ){
     console.log(key);
-    console.log(Performer)
+    var database = firebase.database();
+    var firebaseRef = firebase.database().ref().child("performers/"+key);
+    firebaseRef.on('value', function(snapshot){
+        //console.log(snapshot.val());
+        var obj = snapshot.val();
+        console.log(obj);
+        document.getElementById("heading").innerHTML = "Hello World!";
+    }).then(function(){
+        window.location.reload()
+    }).catch(function(err){});
+    
+
+
+   
+    
 
 
 }
