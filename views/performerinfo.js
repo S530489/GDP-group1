f3be9adf4d0608f024b1054b2885548b44e30c0e
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-11 15:58:11 -0500

    Fetch all performer Info and add bouncing text in performer info page

--- views/performerinfo.js
+++ views/performerinfo.js
@@ -6,12 +6,27 @@ function getInfo(key, performer ){
         //console.log(snapshot.val());
         var obj = snapshot.val();
         console.log(obj);
-        document.getElementById("heading").innerHTML = "Hello World!";
-    }).then(function(){
-        window.location.reload()
-    }).catch(function(err){});
+        document.getElementById("heading").innerHTML = obj.general.Name.First_Name + "'s "+"Information";
+        document.getElementById("FullName").innerHTML = obj.general.Name.First_Name + " "+ obj.general.Name.Last_Name;
+        document.getElementById("age").innerHTML = obj.general.Age;
+        document.getElementById("email").innerHTML = obj.general.Email;
+        document.getElementById("pno").innerHTML = obj.general.Phone[0]+", "+obj.general.Phone[1];
+        document.getElementById("add1").innerHTML = obj.general.Address.Address_line1+ " " +obj.general.Address.Address_line2;
+        document.getElementById("add2").innerHTML = obj.general.Address.City+", "+obj.general.Address.State+", "+obj.general.Address.Zip;
+        document.getElementById("add3").innerHTML = obj.general.Address.Country;
+        document.getElementById("cname").innerHTML = obj.general.Character_Name;
+        document.getElementById("allergy").innerHTML = obj.general.Allergies;
+        document.getElementById("eye").innerHTML = obj.general.EyeColor;
+        document.getElementById("hair").innerHTML = obj.general.HairColor;
+        document.getElementById("ware").innerHTML = obj.general.EyeWare;
+        //window.location.reload();
+    })
+    /*
+    .then(function(){
+        window.location.reload();
+    }) .catch(function(err){});
     
-
+*/
 
    
     
