522d8b132edcd8a07f26cc7f465bb88e6d04dd10
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-09-13 11:30:59 -0500

    Add styling and bootstrap to Designer pull list page

--- views/print.js
+++ views/print.js
@@ -2,14 +2,14 @@
 // link for jspdf documentation https://mrrio.github.io/
 
 function genPDF(){
-    html2canvas(document.getElementById("dplist"),{
+    html2canvas(document.getElementById("dplist1"),{
         onrendered: function (canvas){
             var img = canvas.toDataURL("image/png");
             var doc = new jsPDF("1","mm","a3");
-            doc.setFont("courier");
-            doc.setFontType("bolditalic");
+            doc.setFont("Great Vibes");
+            doc.setFontType("cursive");
             doc.setFontSize(40);
-            doc.setTextColor(26, 140, 255);
+            doc.setTextColor(0, 128, 128);
             doc.text( 'SHOP PULL LIST',90,28,null,null);
     
             doc.addImage(img, 'JPEG', 20,38,);
