1b096ba7a4e6cc377451c0b75368b787267219d6
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-10-26 00:16:42 -0500

    Add functionality to to upload an image of performer from the general form

--- views/getDetails.js
+++ views/getDetails.js
@@ -1,6 +1,9 @@
 // Listen for form submit
 
 var newKey;
+var selectedFile;
+var ProfileImageUrl;
+
 document.getElementById('contactForm').addEventListener('submit', submitForm);
 var perCount = firebase.database().ref().child("perfCount");
 perCount.on('value', function(snapshot){
@@ -8,6 +11,57 @@ perCount.on('value', function(snapshot){
     newKey = snapshot.val()+1;
 
 });
+
+
+document.getElementById("file1").addEventListener("change", funcTest1);
+function funcTest1(){
+    selectedFile = event.target.files;
+    console.log(selectedFile);
+   console.log(selectedFile.name);
+}
+
+
+function uploadTitleImage(){
+    
+    var uploader = document.getElementById("uploadeBar");
+    //document.getElementById("uploadeBar").innerHTML = "Ready to Upload"
+    var fileName = selectedFile[0].name;
+    var storageRef = firebase.storage().ref('/ProfileImages/' + fileName);
+    var uploadTask = storageRef.put(selectedFile[0]);
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', function(snapshot){
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    uploader.value = progress;
+    uploader.setAttribute('data-content', 'Successfully Uploaded : ' +'  '+ progress +"%");
+    //uploader.text = progress+"%";
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
+    }
+  }, function(error) {
+    // Handle unsuccessful uploads
+  }, function() {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+   
+    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      console.log('File available at', downloadURL);
+      ProfileImageUrl = downloadURL
+    });
+  });
+  console.log(ProfileImageUrl)
+}
+
 
 
 // Submit form
