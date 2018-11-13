a1045605d87909ab35a3b7f05be668a92a21fef3
Shrirsha Reddy Rapole <s530484@nwmissouri.edu>
2018-11-13 17:27:48 -0600

    Fix the styling for create user and forgot password

--- test/performerTest.js
+++ test/performerTest.js
@@ -1,11 +1,32 @@
-// process.env.NODE_ENV = 'test'
-// const app = require('../app.js')
-// var performer = require('../views/performerInfo.js')
-// const mocha = require('mocha')
-const expect = require('chai').expect
-// const find = require('lodash.find')
-// const remove = require('lodash.remove')
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
 
-describe("myFunction()", function(){
-    it("just testing...")
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+driver.get('http://127.0.0.1:8081/createaccount');
+
+driver.sleep(5000);
+
+// Checking whether the page opened has the correct title or not
+driver.sleep(2000).then(function() {
+  driver.getTitle().then(function(title) {
+    if(title === 'Costume Designing') {
+      console.log('Matched the title - Test passed');
+    } else {
+      console.log('Test failed');
+    }
+    driver.quit();
+  });
 });
+
+var element = driver.findElement(By.xpath ("//*[@id='new_emailid']"));
+element.sendKeys('Filling in my form');
+//*[@id="new_emailid"]
+
+
