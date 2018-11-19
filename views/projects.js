348efa88311088f7615904348c7ac7a5299e2fce
Shrirsha Reddy Rapole <s530484@nwmissouri.edu>
2018-11-19 17:37:04 -0600

    Add page loader to the projects page

--- views/projects.js
+++ views/projects.js
@@ -60,6 +60,19 @@ $(document).ready(function () {
 
 })
 
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('contents').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('contents').style.visibility="visible";
+        },1000);
+    }
+  }
+
 function getOption() {
     var s = document.getElementsByName("sorting")[0];
     var text = s.options[s.selectedIndex].text;
