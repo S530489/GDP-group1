ef569e7a3ec6bea7e5dc26c430bd8a8b8bda6ee1
Shrirsha Reddy Rapole <s530484@nwmissouri.edu>
2018-11-28 16:45:24 -0600

    Add sort functionality to the shop pull list

--- views/designerPulllist.js
+++ views/designerPulllist.js
@@ -565,3 +565,14 @@ function getSizes(text)
 
     return res;
 }
+
+
+function sorting()
+{
+    var mylist = $('#dplist1');
+    var listitems = mylist.find('tr');
+    listitems.sort(function(a, b) {
+    return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+})
+$.each(listitems, function(idx, itm) { mylist.append(itm); });
+}
